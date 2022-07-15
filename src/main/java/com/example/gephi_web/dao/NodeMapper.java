9e1c70defe50367d0129cac4881f03ef1743717e
Truth360 <tzh19850355091@163.com>
2022-07-15 16:58:07 +0800

    add dealClosure

--- src/main/java/com/example/gephi_web/dao/NodeMapper.java
+++ src/main/java/com/example/gephi_web/dao/NodeMapper.java
@@ -5,12 +5,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Repository
 public class NodeMapper {
@@ -32,7 +31,7 @@ public class NodeMapper {
     public List<CSVNode> search(String type, List<String> nodeNameList) {
         List<CSVNode> nodeList = new ArrayList<>();
         for (String nodeName : nodeNameList) {
-            String sql = "select id, name, attributes from node" + type + " where name= \"" + nodeName+"\"";
+            String sql = "select id, name, attributes from node" + type + " where name= \"" + nodeName + "\"";
             List<Map<String, Object>> queryList = jdbcTemplate.queryForList(sql);
             if (queryList != null && !queryList.isEmpty()) {
                 for (Map<String, Object> map : queryList) {
@@ -85,5 +84,60 @@ public class NodeMapper {
                   COLLATE = utf8mb4_0900_ai_ci;
                 """, graphName);
     }
+
+
+    /**
+     * 目前是两种想法：
+     * 第一种在我们算出来的clousre中的一个包的所有版本的依赖在我们算出来的包对应的某一个版本中才画出来
+     * 第二种是在不在都画出来
+     */
+    /**
+     * 将closure进行处理，找到计算得到的闭包中的包对应的所有版本的序号
+     * 以及这些版本依赖的包的序号（依赖的包也要在closure中），找到对应的版本名，返回出去
+     * @param type 处理的图片的类型 java/python/……
+     * @param closure 计算得到的闭包文件地址
+     * @param requirments requirements文件地址
+     * @return nodeNameList
+     * @throws IOException
+     */
+    public List<String> dealClosure(String type,String closure, String requirments) throws IOException {
+
+        List<String> nodeNameList = new ArrayList<>();
+        // 读取closure file
+        File closureCSV = new File(closure);
+        BufferedReader closureFile = new BufferedReader(new FileReader(closureCSV));
+        Set<String> packageSet = new HashSet<>(); // 记录经过pagerank算法计算后的包的名称
+        String lineDta = "";
+        // 将文档的下一行数据赋值给lineData，并判断是否为空，若不为空则输出
+        while ((lineDta = closureFile.readLine()) != null) {
+            packageSet.add(lineDta);
+        }
+        closureFile.close();
+
+        // 读取原来的requirement文件
+        File requirmentCSV = new File(requirments);
+        BufferedReader requirmentFile = new BufferedReader(new FileReader(requirmentCSV));
+
+        String line = "";
+        while ((line = requirmentFile.readLine()) != null) {
+            if (line.equals("package,edition,requirement,constraint")) continue;
+            // 0 包  1 版本对应的序列号  2 依赖的包  3 依赖包的版本的序列号
+            String[] data = line.split(",");
+            if (packageSet.contains(data[0])) {
+                // 算法一和二的区别就在这，要不要依赖的包也在我们算出来的closure中
+                if (packageSet.contains(data[2])) {
+                    String sql1 = "select name from node" + type + " where id= " + data[1];
+                    String sql2 = "select name from node" + type + " where id= " + data[3];
+                    Map<String, Object> map1=jdbcTemplate.queryForMap(sql1);
+                    nodeNameList.add((String) map1.get("name"));
+                    Map<String, Object> map2=jdbcTemplate.queryForMap(sql2);
+                    nodeNameList.add((String) map2.get("name"));
+                }
+            }
+        }
+        requirmentFile.close();
+        return nodeNameList;
+    }
+
 
 }
--- src/test/java/com/example/gephi_web/Service/ServiceTest.java
+++ src/test/java/com/example/gephi_web/Service/ServiceTest.java
@@ -1,15 +1,12 @@
 package com.example.gephi_web.Service;
 
+import com.example.gephi_web.dao.NodeMapper;
 import com.example.gephi_web.service.GraphService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
@@ -17,20 +14,14 @@ public class ServiceTest {
     @Autowired
     GraphService graphService;
 
+    @Autowired
+    NodeMapper nodeMapper;
+
     @Test
     public void testBuildGraphWithFliter() throws IOException {
         String grahName = "Java";
-        List<String> nodeNameList = new ArrayList<>();
-        File file = new File("/Users/taozehua/Downloads/研究任务/joup/node.csv");
-        BufferedReader edgeFile = new BufferedReader(new FileReader(file));
-        String lineDta;
-        for (int i = 0; i < 5; i++) {
-            lineDta = edgeFile.readLine();
-            if (lineDta != null && lineDta.contains("/")) {
-                String pkg = lineDta.split(",")[1];
-                nodeNameList.add(pkg);
-            }
-        }
+        List<String> nodeNameList = nodeMapper.dealClosure(grahName,"/Users/taozehua/Downloads/研究任务/图谱构建/closure.csv","/Users/taozehua/Downloads/研究任务/图谱构建/requirements.csv");
+
         System.out.println(graphService.searchNodes(grahName, nodeNameList));
     }
 }
